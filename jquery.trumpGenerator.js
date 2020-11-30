ca3120b6bc56640913b6ee1b446d7cedc62a61cb
Adam Ryan <adamryan1983@gmail.com>
2020-11-30 18:27:13 -0330

    cleaned up code
    Eliminated about 10-15 lines of code and cleaned it up

--- jquery.trumpGenerator.js
+++ jquery.trumpGenerator.js
@@ -1,20 +1,11 @@
 (function( $ ){
   $.fn.trumpGenerator = function() {
-    var isValid = true;
-    var quote;
 
-    
+    let isValid = true;
 
-    function generatePersonalQuote(firstName) {
-      var url = 'https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q='+ firstName;
-      fetch(url)
-      .then((resp) => resp.json()) // Transform the data into json
-      .then(function(data) {
-        $('.outputBox').text(data.message)
-        })
-    }
-    function generateRandomQuote() {
-      var url = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+    function generateRandomQuote(firstName) {
+      let url = firstName == null ? 'https://api.whatdoestrumpthink.com/api/v1/quotes/random' : 
+      urlPersonQuote = 'https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q='+ firstName;
       fetch(url)
       .then((resp) => resp.json()) // Transform the data into json
       .then(function(data) {
@@ -22,36 +13,30 @@
         })
     }
 
-    $('.btnSubmit').hover(function() {
-      $(this).css("background-color", "red");
-    },
-    function() {
-      $(this).css("background-color", "blue");
-    })
-    $('.btnSubmit').on("click",function(evt) {
-      var firstName = $(".firstName").val().trim();
+    $('#btnSubmit').on("click",function(evt) {
+      let firstName = $(".firstName").val().trim();
       evt.preventDefault();
       if (firstName == "") { 
         $(".outputBox").text("Name field is required.");
         isValid = false;
       }
       else {
-        generatePersonalQuote(firstName);
-        $(".firstName").val("")
+        generateRandomQuote(firstName);
+        // $(".firstName").val("")
       }
     });
 
-    $('.btnRandom').hover(function() {
+    $('.buttonPress').hover(function() {
       $(this).css("background-color", "red");
     },
     function() {
       $(this).css("background-color", "blue");
     })
-    $('.btnRandom').on("click",function(evt) {
+    $('#btnRandom').on("click",function(evt) {
+      let firstName = null;
       evt.preventDefault();
-      generateRandomQuote();
+      generateRandomQuote(firstName);
     });
-
     
   };
 })( jQuery );
